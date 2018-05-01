e1893c2ec5ef37823f41230e24c8fcabaa3d281b
Glenn-David Daniel <d2rd@outlook.com>
2018-05-01 10:46:53 -0700

    WIP: store and reducer working

--- notes/src/index.js
+++ notes/src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+// 1a) create store 
+import { createStore } from 'redux';
+// 2b) import reducer (2a is create reducer module)
+import { noteReducer } from './noteReducer';
+
+// 1b) create store
+const store = createStore(noteReducer);  //2c add reducer as argument for createStore
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
---
+++ notes/src/noteReducer.js
@@ -0,0 +1,7 @@
+// 2a create reducer(s)
+
+const noteReducer = (notes =[], action) => {
+  return 'foo';
+}
+
+export { noteReducer };
