1b7adc26bfa50f67ebaa11699ffd3044111e6bd7
Glenn-David Daniel <d2rd@outlook.com>
2018-05-10 00:53:15 -0700

    WIP unsuccesful variations of makeNoteSummary in NotesJS

--- notes/src/components/App.js
+++ notes/src/components/App.js
@@ -14,6 +14,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-Title">{`Proximo Notes`}</h1>
           <NoteForm />
         </header>
